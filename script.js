093dcada2a89bd9ea5223db591d3cd950804aec4
Paul <p.rossiter83@gmail.noreply.github.com>
2023-01-28 17:26:28 +0000

    Added a 'operate' function to take in 2 numbers and an operator

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-//create functions for + - * % and tesr in console.log
+//temp prompts to test
+let a = prompt('enter a number');
+let b = prompt('enter another');
+let c = prompt('select an operator')
+let num1 = parseInt(a);
+let num2 = parseInt(b);
+let operator = c;
+console.log('num1',num1);
+console.log('num2', num2)
+console.log('operator used', operator)
+//temp prompts to test
 
-const add = function(a,b) {
-    return a+b
+//OPERATORS
+//create functions for + - * % and test in console.log 
+function add(num1, num2) {
+    let addValue = (num1 + num2);
+    console.log('add', addValue);
+    //return addValue
+}
+function subtract(num1, num2) {
+    let minusValue = (num1 - num2);
+    console.log('minsus', minusValue);
+    //return minusValue    
+}
+function multiply(num1, num2) {
+    let timesValue = (num1 * num2);
+    console.log('times', timesValue) ;
+    //return timesValue
+}
+function divide(num1, num2) {
+    let divideValue = (num1 / num2);
+    console.log('divide', divideValue); 
+    //return divideValue
 };
-console.log(add);
+//TODO - fix bug of float input = NaN decimals
+//OPERATORS
 
-const minus = function(a,b) {
-    return a - b;
-};
-console.log(minus);
-
-const multiply = function(array) {
-    return array.length
-    ? array.reduce((times, nextItem) => times * nextItem )
-    :0;
-};
-console.log(multiply);
-//Create a new function 'operate' that takes an operator & 2 
-//numbers then calls one of the above functions 
-//on the numbers
-
+//2 Create function 'operate' that takes an operator & 2 
+//numbers then calls one of the above functions on the numbers
+//Switch statement?
+function operate(operator, num1, num2) {
+    switch(operator) {
+         case "+":
+             console.log("passing through switch +")
+             add(num1, num2);
+             break;
+         case '-':
+             console.log('passing through switch -')
+             subtract(num1, num2);
+             break;
+         case 'x':
+             console.log('passing through switch x')
+             multiply(num1, num2);
+             break;
+//Display snarky error message if the user tries to divide by 0
+         case '/':
+             console.log('passing through switch /')
+             if(num2 === 0) //return ( "Don't be stupid")
+                 console.log( "Don't be stupid");
+             else divide(num1, num2);
+             break;
+         default:
+             alert("Error didn't recieve operator ");
+    }
+} 
+operate(operator,num1,num2)
+//
 //create basic HTML calc w/btns for each digit, all above ops 
 //An 'equals' btn, A display & a clear(AC) btn.
 
